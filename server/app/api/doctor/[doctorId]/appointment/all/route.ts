df5ac131ebf0d1b133023f5c05a9a4a629532082
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2024-08-22 13:02:01 +0100

    added appointment history

---
+++ server/app/api/doctor/[doctorId]/appointment/all/route.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function PATCH(req: NextRequest) {
+    try {
+        const prisma = new PrismaClient();
+        const appointments = await prisma.appointment.findMany();
+        await prisma.$disconnect();
+        return NextResponse.json(appointments, { status: 200 });
+    } catch (error) {
+        console.log("Error fetching appointment:", error);
+        return NextResponse.json({ error: "Failed to fetch appointment" }, { status: 500 });
+    }
+}
